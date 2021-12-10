44fb7e23a6caa3aa052b874cd9a315d8b4924b72
san k <sankdev0@gmail.com>
2021-12-10 20:50:28 +0500

    Add Files.mismatch() method demo...
    and JUnit Test.

--- src/test/java/com/sankdev/files/content/comparison/FilesMismatchUnitTest.java
+++ src/test/java/com/sankdev/files/content/comparison/FilesMismatchUnitTest.java
@@ -3,7 +3,9 @@ package com.sankdev.files.content.comparison;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class FilesMismatchUnitTest {
@@ -11,10 +13,14 @@ public class FilesMismatchUnitTest {
   public static Path filePath1 = null;
   public static Path filePath2 = null;
 
+  @BeforeAll
+  public static void setup() throws IOException {
+    filePath1 = Files.createTempFile("file1", ".txt");
+    filePath2 = Files.createTempFile("file2", ".txt");
+  }
+
   @Test
   public void givenIdenticalFiles_thenShouldFindMismatch() throws IOException {
-    Path filePath1 = Files.createTempFile("file1", ".txt");
-    Path filePath2 = Files.createTempFile("file2", ".txt");
 
     Files.writeString(filePath1, "Test file contents");
     Files.writeString(filePath2, "Test file contents");
@@ -27,15 +33,18 @@ public class FilesMismatchUnitTest {
   @Test
   public void givenDifferentFiles_thenShouldNotFindMismatch() throws IOException {
 
-    Path filePath1 = Files.createTempFile("file2", ".txt");
-    Path filePath2 = Files.createTempFile("file3", ".txt");
-
     Files.writeString(filePath1, "Test file 1 contents");
     Files.writeString(filePath2, "Test file 2 contents");
 
     long mismatchIdx = Files.mismatch(filePath1, filePath2);
 
     Assertions.assertEquals(10, mismatchIdx, "Should have found mismatch at position 10");
+  }
+
+  @AfterAll
+  public static void cleanup() {
+    filePath1.toFile().deleteOnExit();
+    filePath2.toFile().deleteOnExit();
   }
 
 }
